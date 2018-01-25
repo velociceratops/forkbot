ca55aebadacfb1110b3bfa96ab226c68773801c7
igriffin415 <fireice16@yahoo.com>
2018-01-25 11:21:34 -0500

    io

--- bot.js
+++ bot.js
@@ -1,27 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 var forks = require("./forks.js");
 var theForks = forks.largeArrayFunction();
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
@@ -32,24 +19,20 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             case 'fork':
               var fork = theForks[ (Math.floor(Math.random() * theForks.length)) ];
-              bot.sendMessage({
-                  to: channelID,
-                  message: fork
-              });
+              message.channel.send(fork);
               break;
             case 'iloveisabelle':
-              bot.sendMessage({
-                  to: channelID,
-                  message: 'isabelle is your god now and also the best coder'
-              });
+              message.channel.send("isabelle is your god now and also the best coder");
               break;
             case 'ilovejoss':
-              bot.sendMessage({
-                  to: channelID,
-                  message: 'joss is best mom'
-              });
+              message.channel.send("joss is best mom");
             break;
             // Just add any case commands if you want to..
          }
      }
 });
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
